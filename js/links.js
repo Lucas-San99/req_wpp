c85b18dac3182f51465b0c64414b012ece554527
Pedro Henrique Oliveira Siqueira <ph13633@gmail.com>
2023-11-14 13:59:32 -0300

    add get request to download pages .txt

---
+++ js/links.js
@@ -0,0 +1,8 @@
+function pucButtonClick() {
+  document.getElementById("pucButton").href =
+    "https://www.pucminas.br/unidade/sao-gabriel/Paginas/default.aspx"
+}
+function wppButtonClick() {
+  document.getElementById("whatsappButton").href =
+    "https://wa.me/5531996166591?text=Esse é um teste para vermos o comportamento da API"
+}
